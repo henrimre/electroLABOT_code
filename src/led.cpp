834056cd726c4bc50eee356fa2f96c7434125915
Henri <meureehenri@gmail.com>
2022-02-26 11:04:11 +0100

    blink

--- src/led.cpp
+++ src/led.cpp
@@ -10,9 +10,18 @@ Led::Led(int pin_led)
 }
 */
 
+bool led_state = false;
+unsigned long previous_millis = 0;
+unsigned long current_millis;
+
+char led_d11 = 0x000000;
+char led_d12 = 0x000000;
+char led_d13 = 0x000000;
+char led_d14 = 0x000000;
+
 Led::Led()
 {
-    
+    FastLED.addLeds<WS2812B, DATA_PIN, COLOR_ORDER>(leds,NUM_LEDS);
 }
 
 Led::Led(int pin_led)
@@ -29,3 +38,27 @@ void Led::led_on(int pin_led)
 {
     digitalWrite(pin_led, HIGH);
 }
+
+void Led::rgb_blink(unsigned long blink_time)
+{
+    current_millis = millis();
+    
+    if((current_millis - previous_millis) >= blink_time)
+    {
+        Serial.print("Toogle led");
+        previous_millis = millis();
+
+        for (int i = 0; i < 4; i++)
+        {
+            if(led_state == true) leds[i] = 0x000000;
+            else leds[i] = CRGB::Tomato;
+        }
+
+        FastLED.show();
+        
+        if(led_state == true) led_state = false;
+        else led_state = true;
+    }
+    
+}
+
--- src/led.h
+++ src/led.h
@@ -1,19 +1,36 @@
-#include <elabot.h>
-
 #ifndef LED
 #define LED
+
+//#include <elabot.h>
+#include <FastLED.h>
+
+//Nombre de LED sur l'ELABOT
+#define NUM_LEDS    4 
+#define DATA_PIN    32
+#define LED_D11     0
+#define LED_D12     1
+#define LED_D13     2
+#define LED_D14     3
+#define COLOR_ORDER GRB
+
 
 class Led
 {
     private : 
+        void rgb_init();
+        CRGB leds[NUM_LEDS];
+
 
     public : 
         Led();
         Led(int pin_led);
+
         void led_off(int pin_led);
         void led_on(int pin_led);
 
+        void rgb_blink(unsigned long blink_time);
+        void rgb_set_color(char led, int R, int G, int B);
+    
 };
-
 
 #endif
