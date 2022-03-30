f91ead0fcfea44c55953ca164aa8610dc1f2b8b0
henrimre <95986228+henrimre@users.noreply.github.com>
2022-03-30 19:14:09 +0200

    merge resolving problem

--- src/head.h
+++ src/head.h
@@ -6,28 +6,16 @@
 
 #define pin_servo       13
 #define pin_ultrason    27
-<<<<<<< HEAD
-#define LEFT_FREE       1
-#define RIGHT_FREE      2
-#define NO_FREE         3
-#define KEEP_GOING      0
-#define STOP            1
-#define PROBLEM         3
-=======
 
->>>>>>> head_
-
+const char LEFT_FREE = 1;
+const char RIGHT_FREE = 2;
+const char NO_FREE = 3;
+const char KEEP_GOING = 0;
+const char STOP = 4;
+const char PROBLEM = 5;
 class Head
 {
     private : 
-<<<<<<< HEAD
-=======
-        const char LEFT_FREE = 1;
-        const char RIGHT_FREE = 2;
-        const char NO_FREE = 3;
-        const char KEEP_GOING = 4;
-        const char STOP = 5;
->>>>>>> head_
     public : 
         Head();
         void move_head(int);
